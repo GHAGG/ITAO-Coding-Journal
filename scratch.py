0e473792de692697460165a018c6e412970fd34b
Agossmanngonzalez <89820548+GHAGG@users.noreply.github.com>
2021-09-28 00:20:03 -0400

    Add files via upload
    Added functionality to scratch

--- scratch.py
+++ scratch.py
@@ -1,23 +1,44 @@
 from mod.drinks import *
 
 drinktypes = ["Sprite", "Water", "Coke"]
+YN = ["Y", "N"]
+drinksordered = []
 allcal = []
 
-drink = input("Which drink would you like today? ")
-try:
-    drink = str(drink)
-except:
-    "Invalid Input"
-else:
-    if "Diet" in drink.split().capitalize:
-        pass
-    drink = drink.capitalize()
-    if drink in drinktypes:
-        if (drink == "Sprite"):
-            drink = Sprite()
-        elif (drink == "Coke"):
-            drink = Coke()
-        elif (drink == "Water"):
-            drink = Drink()
-        print("A {} has {} calories".format(drink.syrup, drink.cal()))
-        allcal.append(drink.cal())
+
+while True:
+    try:
+        drink = str(input("Which drink would you like today? "))
+    except:
+        "Invalid Input"
+    else:
+        drink = drink.capitalize()
+        if "Diet" in drink.split():
+            if (drink == "Diet Coke"):
+                drink = DietCoke()
+            elif (drink == "Diet Sprite"):
+                drink = DietSprite()
+            print("A {} has 0 calories".format(drink))
+            allcal.append(0)
+            drinktypes.append(drink)
+        elif drink in drinktypes:
+            if (drink == "Sprite"):
+                drink = Sprite()
+            elif (drink == "Coke"):
+                drink = Coke()
+            elif (drink == "Water"):
+                drink = Drink()
+            print("A {} has {} calories".format(drink.syrup, drink.cal()))
+            allcal.append(drink.cal())
+            drinktypes.append(drink.syrup)
+        another = str(input("Would you like to order another drink? (Y/N) "))
+        try:
+            another.capitalize() in YN
+        except:
+            print("Please enter 'Y' or 'N' as a response")
+        else:
+            another.capitalize()
+            if (another.capitalize() == 'Y'):
+                continue
+            else:
+                break
